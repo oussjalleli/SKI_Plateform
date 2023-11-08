e435188eadd33072defb31cccf1626b05d3e3537
Oussama Jalleli <oussama.jalleli@esprit.tn>
2023-11-08 17:32:08 +0100

    Monitoring

--- startup-script.sh
+++ startup-script.sh
@@ -7,7 +7,7 @@ ARTIFACT_ID="SkiStationProject"
 VERSION="1.0.0"
 
 # Fetch the artifact from the Nexus repository
-curl -o ski.jar "$REPO_URL/$GROUP_ID/$ARTIFACT_ID/$VERSION/$ARTIFACT_ID-$VERSION.jar"
+curl -o "$REPO_URL/$GROUP_ID/$ARTIFACT_ID/$VERSION/$ARTIFACT_ID-$VERSION.jar" ski.jar
 
 # Run your Java application with the fetched JAR
 java -jar ski.jar
